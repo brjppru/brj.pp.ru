0e0d5aac62da5d0312e928636cf12438bc0d1184
Roman Y. Bogdanov <sam@brj.pp.ru>
2017-09-24 02:54:32 +0700

    2017-09-24-025431 It was the best of times, it was the worst of times

---
+++ commit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# git push imperfectioin.ru repo to github
+
+dated=`date +%Y-%m-%d-%H%M%S`
+
+# regen 404
+cat assets/404/404.html > 404.html
+cat index.html >> 404.html
+
+# git add
+
+git add .
+echo run git push on ${dated}
+
+for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
+
+git commit -m "$dated $(curl -s http://whatthecommit.com/index.txt)"
+git push -u origin master
+
